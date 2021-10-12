f0f20af895361a6062730bbe14d628e27e797bd5
Paulinho <qualvalordex@gmail.com>
2021-10-12 12:44:34 -0300

    :sparkles: metodo para calcular a raiz aproximada de uma funçao polinomial

--- js/Matematica/CalculoNumerico/funcoes.js
+++ js/Matematica/CalculoNumerico/funcoes.js
@@ -9,4 +9,26 @@ function polyval (coefs, x) {
     return parseFloat(result.toFixed(4))
 }
 
-module.exports = { polyval }
+function bissec (coefs, a, b, tol, nmax) {
+    let pm = 0 // pm = ponto médio
+    for (let n = 1; n <= nmax; n++) {
+        const fa = polyval(coefs, a) // Calcular f(a)
+        const fb = polyval(coefs, b) // Calcular f(b)
+
+        if (fa * fb > 0) return NaN // Garante que o intervalo possui uma raíz
+
+        pm = (a + b)/2 // Cálculo do ponto médio do intervalo
+        const fpm = polyval(coefs, pm) // Calcular f(pm)
+        
+        if (fpm === 0 || Math.abs((b-a))/2 < tol) return parseFloat(pm.toFixed(4)) // Solução encontrada
+
+        if (fa * fpm > 0) { // Verifica se f(a) e f(pm) possuem o mesmo sinal
+            a = pm
+        } else {
+            b = pm
+        }
+    }
+    return parseFloat(pm.toFixed(4))
+}
+
+module.exports = { polyval, bissec }
--- js/Matematica/CalculoNumerico/funcoes.test.js
+++ js/Matematica/CalculoNumerico/funcoes.test.js
@@ -1,7 +1,13 @@
-const { polyval } = require('./funcoes.js')
+const { polyval, bissec } = require('./funcoes.js')
 
 // Polyval tests
 console.log('Testing polyval...')
 console.log('Se f(x) = x + 3, para x=2 espera-se f(2) = 5: ', polyval([1, 3], 2) === 5)
 console.log('Se f(x) = x^2 + 3x + 10, para x=0, espera-se f(0) = 10: ', polyval([1, 3, 10], 0) === 10)
 console.log('Se f(x) = 2x^5 + 6x^4 + x^3 + 4x + 1, para x=2.3, espera-se f(2.3) = 318.9985: ', polyval([2, 6, 1, 0, 4, 1], 2.3) === 318.9985)
+
+// Bissec tests
+console.log('Testing bissec...')
+console.log('Se f(x) = 2x + 1, para o intervalo [-2,2], espera-se x=-0.5: ', bissec([2, 1], -2, 2, 0.0001, 20) === -0.5)
+console.log('Se f(x) = x^2 - x - 2, para o intervalo [0,5], para 10 iterações, espera-se x=1.9971: ', bissec([1, -1, -2], 0, 5, 0.0001, 10) === 1.9971)
+console.log('Se f(x) = 0.28x^4 + 2x^2 - 4x -100, para o intervalo [0,10], para 30 iterações, espera-se x=4.1402: ', bissec([0.28, 0, 2, -4, -100], 0, 10, 0.0001, 30) === 4.1402)
